d1822cc7f8f710b604d6f73b5cd64dd2d7762708
sriramsv <sriramsv1991@gmail.com>
2016-05-19 13:58:19 -0400

    final commit on presentation day

--- gcm.sh
+++
@@ -1,6 +0,0 @@
-api_key="AIzaSyC_AVsFIucdoHx7wXAvgkYvEiPAlzAAQzg"
-
-curl --header "Authorization: key=$api_key" \
-       --header Content-Type:"application/json" \
-       https://gcm-http.googleapis.com/gcm/send \
-       "
